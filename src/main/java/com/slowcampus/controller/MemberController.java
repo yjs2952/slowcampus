6a4c17b50886e3182b67878dd1fd52d13fea7c53
Taehee Jang <jang0913@gmail.com>
2018-10-30 15:38:54 +0900

    MemberService 생성과 MemberController로 Refactoring

--- src/main/java/com/slowcampus/controller/MemberController.java
+++ src/main/java/com/slowcampus/controller/MemberController.java
@@ -8,18 +8,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @Log
-public class UserController {
+public class MemberController {
 
     @RequestMapping(value = "/signup", method = RequestMethod.GET)
     public String signupMember() {
+        log.info("회원 가입 요청이 수행되었습니다.");
         return "user/signup";
     }
 
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
     public void signupMember(Member member) {
-        System.out.println(member.getId());
-        System.out.println(member.getPassword());
-        System.out.println(member.getNickname());
-        System.out.println(member.getEmail());
+        log.info("회원 가입 POST 요청을 Member 객체로 받았습니다.");
+
     }
 }
